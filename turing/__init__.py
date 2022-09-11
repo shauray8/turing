97236e66bebaa7a6b23f45b392c389da59b31213
Shauray <shauray.209309012@muj.manipal.edu>
2022-09-11 16:26:11 +0530

    quite some work

--- turing/__init__.py
+++ turing/__init__.py
@@ -1,1 +1,2 @@
 from turing import tensor
+
--- turing/tensor.py
+++ turing/tensor.py
@@ -2,7 +2,7 @@ import numpy as np
 from typing import Optional
 
 class Tensor:
-  def __init__(self,data,requires_grad=True,_children=()):
+  def __init__(self,data,requires_grad=False,_children=()):
     self.data = data.astype('float32') if isinstance(data, np.ndarray) else np.array(data, dtype=np.float32)
     self.grad = 0.0
     self.requires_grad = requires_grad
@@ -10,10 +10,10 @@ class Tensor:
     self._backward = lambda : None
 
   def __repr__(self):
-    return f"Tensor({self.data} with grad {self.grad if self.requires_grad else None})"
+    return f"Turing_Tensor({self.data}, requires_grad={True if self.requires_grad else False})"
 
   def __add__(self, other):
-    other = other if isinstance(other, Tensor) else Tensor(other)
+    other = other if isinstance(other, Tensor) else Tensor(other, requires_grad = self.requires_grad)
     return Tensor(self.data+other.data)
 
     def _backward():
@@ -24,14 +24,24 @@ class Tensor:
 
   def __mul__(self, other):
     other = other if isinstance(other, Tensor) else Tensor(other)
-    return Tensor(self.data * other.data)
+    return Tensor(self.data * other.data,requires_grad = (self.requires_grad == True) or (other.requires_grad == True))
 
   def __rmull__(self, other):
     return self*other
 
+  def __neg__(self):
+    return self * -1
+
   def __sub__(self, other):
-    other = other if isinstance(other, Tensor) else Tensor(other)
-    return Tensor(self.data - other.data)
+    return self + (-other)
+
+  def __truediv__(self, other):
+    return self * other ** -1
+
+  def __pow__(self, other):
+    assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+    out = Tensor(self.data**other, (self,),f"**{other}")
+    return out
 
   def backward():
     ## some tree node shit gonna copy and paste from karpathy's micrograd
@@ -44,11 +54,17 @@ class Tensor:
 
   def arange(start,stop,step=1,**kwargs): return Tensor(np.arange(start=start, stop=stop, step=step, dtype=np.float32),**kwargs)
 
-if __name__ == "__main__":
-  print(Tensor.arange(-5,5,.2,requires_grad=False))
-      
-  a = 10+Tensor(10)
-  a._backward()
-  print(a)
+  def is_turing_tensor(self): return True if isinstance(self, Tensor) else False
 
+  def ones(size, **kwargs): return Tensor(np.ones(size),**kwargs)
+    
+
+if __name__ == "__main__":
+
+  a = Tensor(10,requires_grad=True)
+  b = Tensor(99,requires_grad=True)
+  c = a + b
+  d = c * a**2
+  e = d / b
+  print(e)
     
