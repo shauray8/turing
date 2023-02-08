b777cd059349924d19f55a16fa9e6c6fb2376986
shauray8 <shauray.209309012@muj.manipal.edu>
2023-02-09 01:56:58 +0530

    stick with numpy, its way better

---
+++ Aten/gemm.py
@@ -0,0 +1,18 @@
+## this sucker produces 120 GFLOPS !! (what does numpy uses for matrix multiplication ??) 
+import numpy as np
+import time
+
+N = 1024
+
+A = np.random.randn(N,N).astype(np.float32)
+B = np.random.randn(N,N).astype(np.float32)
+
+for i in range(100):
+  st = time.monotonic()
+  res = A@B
+  et = time.monotonic()
+  gflop = (N*N*2.0*N)*1e-9
+  s = et-st
+
+  print(f"{gflop/s= :.4f}")
+
