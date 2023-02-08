8c25e8ecfabae7359da28abed98faeee90d514f6
shauray8 <shauray.209309012@muj.manipal.edu>
2023-02-09 01:45:16 +0530

    runs better on multiple thread

--- Aten/gemm.cpp
+++ Aten/gemm.cpp
@@ -3,16 +3,22 @@
 #include <iostream>
 #include <omp.h>
 #include <time.h>
+#include <vector>
 
 #define N 1024
 #define ll long long
+#define NUM_WORKERS 8
 
 using namespace std;
+
+// slows the performance down !
+//vector<vector<float>> A(N, vector<float>(N,0));
+//vector<vector<float>> B(N, vector<float>(N,0));
+//vector<vector<float>> res(N, vector<float>(N,0));
 
 float A[N][N];
 float B[N][N];
 float res[N][N];
-
 
 int64_t nanos() {
     struct timespec start;
@@ -30,15 +36,32 @@ void static_v1(){
       }
     }
   }
+  for(int i=0; i<N; i++){
+    for(int j=0; j<N; j++){
+        printf("%f\n",res[i][j]);
+    }
+  }
 }
 
+// g++ -fopenmp gemm.cpp -o gemm && ./gemm
+// I don't actually know if it's right or not but it runs on all the threads and gives off: 1.350512
 void dynamic_v1(){
-  ;
+  float acc = 0;
+  #pragma omp parallel for schedule(static,NUM_WORKERS) reduction(+:acc)
+  for(int i=0; i<N; i++){
+    for(int j=0; j<N; j++){
+      acc = 0;
+      for(int k=0; k<N; k++){
+        acc += A[i][k] * B[k][j];
+      }
+      res[i][j] = acc;
+    }
+  }
 }
 
 int main(){
   uint64_t start = nanos();
-  static_v1();
+  dynamic_v1();
   uint64_t end = nanos();
   double time = double(end-start)*1e-9;
   double flop = (N*N*2.0*N) *1e-9;
