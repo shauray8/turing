f5defd37fb26c0c86859fc0e1a404ad0e75dda12
shauray8 <shauray.209309012@muj.manipal.edu>
2023-02-09 00:30:59 +0530

    getting gemm

---
+++ Aten/gemm.cpp
@@ -0,0 +1,17 @@
+// General matrix multiplication benchmarking 
+#include <iostream>
+#include <omp.h>
+
+using namespace std;
+
+void static_v1(){
+  ;
+}
+
+void static_v1(){
+  ;
+}
+
+int main(){
+  ;
+}
