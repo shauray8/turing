f940686dbf0289f74159bade07828f33fd5c7e95
shauray8 <shauray.209309012@muj.manipal.edu>
2023-02-09 01:13:29 +0530

    runs on a single thread

--- Aten/gemm.cpp
+++ Aten/gemm.cpp
@@ -4,27 +4,28 @@
 #include <omp.h>
 #include <time.h>
 
-#define N 2048
+#define N 1024
 #define ll long long
 
 using namespace std;
 
-ll A[N][N];
-ll B[N][N];
-ll res[N][N];
+float A[N][N];
+float B[N][N];
+float res[N][N];
 
 
 int64_t nanos() {
     struct timespec start;
     clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    return (uint64_t)start.tv_sec + (uint64_t)start.tv_nsec;
+    return (uint64_t)start.tv_sec*1000000000 + (uint64_t)start.tv_nsec;
 }
 
+// runs on a single thread (pathetic performance !! gives off: 0.177351 GFLOP/s)
 void static_v1(){
   for(int i=0; i<N; i++){
-    for(int j=0; i<N; i++){
+    for(int j=0; j<N; j++){
       res[i][j] = 0;
-      for(int k=0; i<N; i++){
+      for(int k=0; k<N; k++){
         res[i][j] += A[i][k] * B[k][j];
       }
     }
@@ -40,6 +41,6 @@ int main(){
   static_v1();
   uint64_t end = nanos();
   double time = double(end-start)*1e-9;
-  double flop = N*N*2.0*N *10e-9;
-  printf("FLOP/s: %f",time);
+  double flop = (N*N*2.0*N) *1e-9;
+  printf("GFLOP/s: %f\n",flop/time);
 }
