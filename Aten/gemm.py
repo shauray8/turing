2df48622b8071556d77c5b16eaa1705935d920db
shauray8 <shauray.209309012@muj.manipal.edu>
2023-02-16 02:23:09 +0530

    we have cuda

--- Aten/gemm.py
+++ Aten/gemm.py
@@ -3,15 +3,11 @@
 import numpy as np
 import os
 import time
-import torch
 
 N = 4096
 
 A = np.random.randn(N,N).astype(np.float32)
 B = np.random.randn(N,N).astype(np.float32)
-
-A1 = torch.randn(N,N, dtype=torch.float32).to("cuda")
-B1 = torch.randn(N,N, dtype=torch.float32).to("cuda")
 
 for i in range(1):
   st = time.monotonic()
@@ -26,14 +22,4 @@ with open("./tmp/data","wb") as f:
   f.write(A.data)
   f.write(B.data)
   f.write(res.data)
-
-for i in range(1):
-  st = time.monotonic()
-  res = torch.matmul(A1,B1)
-  et = time.monotonic()
-  gflop = (N*N*2.0*N)*1e-9
-  s = et-st
-
-  print(f"torch : {gflop/s= :.4f}")
-
 
