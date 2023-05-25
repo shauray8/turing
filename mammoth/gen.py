6b55b89bbc1c43ba2d0aa1a57797aac36f479c73
shauray8 <shauray.209309012@muj.manipal.edu>
2023-05-25 20:58:03 +0530

    matmul

--- mammoth/gen.py
+++ mammoth/gen.py
@@ -3,7 +3,7 @@ import os
 import time
 from scipy import signal
 
-N = 128
+N = 1024
 K = 3
 
 A = np.random.randn(N,N).astype(np.float32)
@@ -13,18 +13,22 @@ C = np.zeros((N,N)).astype(np.float32)
 ops = (N-1)*(N-1)*K*K*2.0*K # does not consider padding
 
 st = time.monotonic()
-for i in range(N):
-  for j in range(N):
+gl = 0
+for i in range(N-1):
+  for j in range(N-1):
     temp = 0
     for k in range(K):
       for l in range(K):
+        gl += 2
         if i+k < N and j+l < N:
           temp += A[i+k][j+l] * B[k][l]
 
     C[i][j] = temp
+    gl += 1
 et = time.monotonic()
 gflop = ops*1e-9
 s = et-st
+print(ops, gl)
 print(f"{gflop/s= :.4f}")
 
 """
