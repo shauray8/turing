6ba2be5636204d144cc7cc707df8edf75866e066
shauray8 <shauray.209309012@muj.manipal.edu>
2023-05-30 09:46:37 +0530

    still no progress

--- mammoth/wino.cpp
+++ mammoth/wino.cpp
@@ -1,86 +1,79 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 
-// Function to generate the F matrix for Winograd convolution
-std::vector<std::vector<double>> generateFMatrix(int N, int K, int M) {
-    std::vector<std::vector<double>> F(M + K - 1, std::vector<double>(N, 0.0));
-    double alpha = 1.0 / std::sqrt(K * M);
-
-    for (int i = 0; i < M + K - 1; ++i) {
-        for (int j = 0; j < N; ++j) {
-            if (i < K && i + j < N) {
-                F[i][j] = alpha;
-            } else if (i >= K && i - K + 1 + j < N) {
-                F[i][j] = alpha * (1.0 - (i - K + 1));
-            }
-        }
-    }
-
-    return F;
-}
-
-// Function to generate the G matrix for Winograd convolution
-std::vector<std::vector<double>> generateGMatrix(int N, int K, int M) {
-    std::vector<std::vector<double>> G(K + M - 1, std::vector<double>(K, 0.0));
-    double beta = 1.0 / std::sqrt(K * M);
-
-    for (int i = 0; i < K + M - 1; ++i) {
-        for (int j = 0; j < K; ++j) {
-            if (i < K && i + j < K) {
-                G[i][j] = beta;
-            } else if (i >= K && i - K + 1 + j < K) {
-                G[i][j] = beta * (1.0 - (i - K + 1));
-            }
-        }
-    }
-
-    return G;
-}
-
-// Function to perform Winograd convolution
 std::vector<std::vector<double>> winogradConvolution(const std::vector<std::vector<double>>& input,
-                                                    const std::vector<std::vector<double>>& kernel,
-                                                    const std::vector<std::vector<double>>& F,
-                                                    const std::vector<std::vector<double>>& G) {
-    int N = input.size();  // Input size
-    int K = kernel.size(); // Kernel size
-    int M = input.size() - kernel.size() + 1; // Output size
+                                                    const std::vector<std::vector<double>>& kernel) {
+    int N = input.size();        // Input size
+    int K = kernel.size();       // Kernel size
+    int M = N - K + 1;           // Output size
 
-    // Perform F-transform on input
-    std::vector<std::vector<double>> transformedInput(M + K - 1, std::vector<double>(N, 0.0));
-    for (int i = 0; i < M + K - 1; ++i) {
-        for (int j = 0; j < N; ++j) {
-            for (int k = 0; k < K; ++k) {
-                transformedInput[i][j] += F[i][k] * input[k][j];
-            }
+    // Calculate D matrix
+    std::vector<std::vector<double>> D(N+K-1, std::vector<double>(K, 0.0));
+    D[0][0] = 1.0;
+    D[1][0] = 1.0;
+    D[2][0] = 1.0;
+    for (int i = 3; i < K + 2; ++i) {
+        D[i][0] = -1.0 / (1 << (i - 2));
+    }
+
+    // Calculate B matrix
+    std::vector<std::vector<double>> B(N+K-1, std::vector<double>(N, 0.0));
+    for (int i = 2; i < N + 2; ++i) {
+        for (int j = 2; j < N + 2; ++j) {
+            B[i][j] = input[i - 2][j - 2];
         }
     }
 
-    // Perform G-transform on kernel
-    std::vector<std::vector<double>> transformedKernel(K + M - 1, std::vector<double>(K, 0.0));
-    for (int i = 0; i < K + M - 1; ++i) {
+    // Calculate A matrix
+    std::vector<std::vector<double>> A(M, std::vector<double>(N+K-1, 0.0));
+    for (int i = 0; i < M; ++i) {
+        for (int j = 2; j < M + 2; ++j) {
+            A[i][j] = 1.0;
+        }
+    }
+
+    // Perform Winograd convolution
+    std::vector<std::vector<double>> transformedInput(K, std::vector<double>(K, 0.0));
+    std::vector<std::vector<double>> transformedKernel(K, std::vector<double>(K, 0.0));
+    std::vector<std::vector<double>> transformedOutput(M, std::vector<double>(M, 0.0));
+
+    // Transform input to Winograd domain
+    for (int i = 0; i < K; ++i) {
         for (int j = 0; j < K; ++j) {
             for (int k = 0; k < K; ++k) {
-                transformedKernel[i][j] += G[i][k] * kernel[k][j];
+                transformedInput[i][j] += B[i][k] * input[k][j];
             }
         }
     }
 
-    // Perform element-wise multiplication in Winograd domain
-    std::vector<std::vector<double>> multiplied(M + K - 1, std::vector<double>(M + K - 1, 0.0));
-    for (int i = 0; i < M + K - 1; ++i) {
-        for (int j = 0; j < M + K - 1; ++j) {
-            multiplied[i][j] = transformedInput[i][j] * transformedKernel[i][j];
+    // Transform kernel to Winograd domain
+    for (int i = 0; i < K; ++i) {
+        for (int j = 0; j < K; ++j) {
+            for (int k = 0; k < K; ++k) {
+                transformedKernel[i][j] += D[i][k] * kernel[k][j];  // Fixed the indices here
+            }
         }
     }
 
-    // Perform inverse F-transform on multiplied values
-    std::vector<std::vector<double>> output(M, std::vector<double>(M, 0.0));
+    // Perform element-wise multiplication in the Winograd domain
     for (int i = 0; i < M; ++i) {
         for (int j = 0; j < M; ++j) {
             for (int k = 0; k < K; ++k) {
-                output[i][j] += 1.0/F[i][k] * multiplied[k][j];
+                for (int l = 0; l < K; ++l) {
+                    transformedOutput[i][j] += transformedInput[k][l] * transformedKernel[i + k][j + l];  // Fixed the indices here
+                }
+            }
+        }
+    }
+
+    // Transform output back to spatial domain using A matrix
+    std::vector<std::vector<double>> output(N, std::vector<double>(N, 0.0));
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            for (int k = 0; k < M; ++k) {
+                for (int l = 0; l < M; ++l) {
+                    output[i][j] += A[k][i] * transformedOutput[k][l] * A[l][j];
+                }
             }
         }
     }
@@ -91,23 +84,16 @@ std::vector<std::vector<double>> winogradConvolution(const std::vector<std::vect
 int main() {
     // Example usage
     std::vector<std::vector<double>> input = {
-        {3, 2, 3},
+        {1, 2, 3},
         {4, 5, 6},
-        {7, 8, 1}
+        {7, 8, 9}
     };
     std::vector<std::vector<double>> kernel = {
         {1, 0},
-        {0, 0}
+        {0, 1}
     };
 
-    int N = input.size();  // Input size
-    int K = kernel.size(); // Kernel size
-    int M = input.size() - kernel.size() + 1; // Output size
-
-    std::vector<std::vector<double>> F = generateFMatrix(N, K, M);
-    std::vector<std::vector<double>> G = generateGMatrix(N, K, M);
-
-    std::vector<std::vector<double>> output = winogradConvolution(input, kernel, F, G);
+    std::vector<std::vector<double>> output = winogradConvolution(input, kernel);
 
     // Print the output
     std::cout << "Output:\n";
